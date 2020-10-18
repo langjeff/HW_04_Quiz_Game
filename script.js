60900f803640b849832c141e2f5e1a2a023d31c9
IronJon <lang.jeffrey@gmail.com>
2020-10-17 22:16:25 -0600

    Completed functionality for questions and answer alerts

--- script.js
+++ script.js
@@ -30,9 +30,9 @@ var questions = [
     answer: "Mordor",
     meme: "./assets/mordor.jpg"    
     },
-    {question: "What is my name?",
-    choices: ["Kevin", "Tracy", "Vince", "Shawn"],
-    answer: "Kevin",
+    {question: "What team do I play for?",
+    choices: ["Brooklyn Nets", "Golden State Warriors", "Toronto Raptors", "Cleveland Caviliers"],
+    answer: "Brooklyn Nets",
     meme: "./assets/mvp.jpg"    
     },
     {question: "I am a:",
@@ -63,6 +63,7 @@ var userScore = 0;
 var choicesArray = questions[questionNumber].choices;
 var finalScore;
 var highScores = [];
+var quizTime = 100;
 
 // EVENT LISTENER AND FUNCTIONS FOR QUIZ QUESTIONS
 startEl.addEventListener("click", function () {
@@ -103,30 +104,60 @@ function createChoices() {
         var userChoice = event.target.textContent;
         // evaluate if answer is right
         if(userChoice === questions[questionNumber].answer) {
-            // console.log("you are right"); //console log to check right answer
-            //create alert for right answer & append
-            var correctChoice = document.createElement("div");
-            correctChoice.setAttribute("role","alert");
-            correctChoice.setAttribute("class","alert alert-success");
-            correctChoice.setAttribute("style","width: 50%; margin: auto; padding: 1em");
-            correctChoice.textContent = "That's right. You got 10 points!";
-            choicesEl.appendChild(correctChoice);
-            // increment userScore by 10
-            userScore += 10;  
-            // console.log(userScore);   
+            //increase time by 5 seconds
+            quizTime += 5;
+            //increment question number and see if any questions remain            
+            questionNumber ++;
+            // conditional for end of questions
+            if (questionNumber <= (questions.length - 1)) {
+                choicesEl.innerHTML = "";
+                getQuestions();
+                correct();
+            } else {
+                choicesEl.innerHTML = "";
+                correct();
+                //code to display finalScore & form to save
+            }
+            
         } 
         else {
-            console.log("you are wrong");
-            //create alert for right answer
-            var incorrectChoice = document.createElement("div");
-            incorrectChoice.setAttribute("role","alert");
-            incorrectChoice.setAttribute("class","alert alert-danger");
-            incorrectChoice.setAttribute("style","width: 50%; margin: auto; padding: 1em");
-            incorrectChoice.textContent = "That's incorrect. You lose 5 seconds.";
-            choicesEl.appendChild(incorrectChoice);
+            //decrease time by 10 seconds
+            quizTime -= 10;
+            //increment question number and see if any questions remain            
+            questionNumber ++;
+            // conditional for end of questions
+            if (questionNumber <= (questions.length - 1)) {
+                choicesEl.innerHTML = "";
+                getQuestions();
+                incorrect();
+            } else {
+                choicesEl.innerHTML = "";
+                correct();
+                //code to display finalScore & form to save
+            }
+                
+
         } 
-        questionNumber ++;
-        getQuestions();
+        
     }
 })
+
+// FUNCTION FOR CORRECT CHOICE
+function correct () {
+    var correctChoice = document.createElement("div");
+    correctChoice.setAttribute("role","alert");
+    correctChoice.setAttribute("class","alert alert-success");
+    correctChoice.setAttribute("style","width: 50%; margin: auto; padding: 1em");
+    correctChoice.textContent = "That's right. You gain 5 seconds!";
+    choicesEl.appendChild(correctChoice);
+}
+
+function incorrect () {
+    var incorrectChoice = document.createElement("div");
+    incorrectChoice.setAttribute("role","alert");
+    incorrectChoice.setAttribute("class","alert alert-danger");
+    incorrectChoice.setAttribute("style","width: 50%; margin: auto; padding: 1em");
+    incorrectChoice.textContent = "That's incorrect. You lose 10 seconds!";
+    choicesEl.appendChild(incorrectChoice);
+}
 
