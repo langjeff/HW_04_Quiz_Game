e3be692e8c4b95c5fc1396692b90f4803dd09d55
IronJon <lang.jeffrey@gmail.com>
2020-10-19 21:55:56 -0600

    Worked with tutor to get local storage working. Made styling updates for buttons

--- script.js
+++ script.js
@@ -5,51 +5,52 @@ var questions = [
     answer: "Distracted Boyfriend",
     meme: "./assets/distracted.jpg"    
     },
-    // {question: "What is my name?",
-    // choices: ["Dog", "Shiba", "Doge", "Cat"],
-    // answer: "Doge",
-    // meme: "./assets/doge.png"    
-    // },
-    // {question: "Where did this meme come from?",
-    // choices: ["Futurama", "Rick & Morty", "American Dad", "Family Guy"],
-    // answer: "Family Guy",
-    // meme: "./assets/farm.jpg"    
-    // },
-    // {question: "Greg is a...",
-    // choices: ["Good Guy", "Bad Guy", "Stoned Guy", "Philosoraptor"],
-    // answer: "Good Guy",
-    // meme: "./assets/greg.jpg"    
-    // },
-    // {question: "My nickname is:",
-    // choices: ["Mike", "Jump", "Air", "King"],
-    // answer: "Air",
-    // meme: "./assets/jordan.jpg"    
-    // },
-    // {question: "Where is he walking into?",
-    // choices: ["DisneyLand", "Sauron", "Winterfell", "Mordor"],
-    // answer: "Mordor",
-    // meme: "./assets/mordor.jpg"    
-    // },
-    // {question: "What team do I play for?",
-    // choices: ["Brooklyn Nets", "Golden State Warriors", "Toronto Raptors", "Cleveland Caviliers"],
-    // answer: "Brooklyn Nets",
-    // meme: "./assets/mvp.jpg"    
-    // },
-    // {question: "I am a:",
-    // choices: ["Diknowitall", "Jurassiphiser", "Sageasauras", "Philosoraptor"],
-    // answer: "Philosoraptor",
-    // meme: "./assets/raptor.jpg"    
-    // },
-    // {question: "Come with me, and you see, a world of...",
-    // choices: ["Much Procastination", "Pure Imagination", "Sad Lamentation", "Infinite Experimentation"],
-    // answer: "Pure Imagination",
-    // meme: "./assets/willy.jpg"    
-    // },
+    {question: "What is my name?",
+    choices: ["Dog", "Shiba", "Doge", "Cat"],
+    answer: "Doge",
+    meme: "./assets/doge.png"    
+    },
+    {question: "Where did this meme come from?",
+    choices: ["Futurama", "Rick & Morty", "American Dad", "Family Guy"],
+    answer: "Family Guy",
+    meme: "./assets/farm.jpg"    
+    },
+    {question: "Greg is a...",
+    choices: ["Good Guy", "Bad Guy", "Stoned Guy", "Philosoraptor"],
+    answer: "Good Guy",
+    meme: "./assets/greg.jpg"    
+    },
+    {question: "My nickname is:",
+    choices: ["Mike", "Jump", "Air", "King"],
+    answer: "Air",
+    meme: "./assets/jordan.jpg"    
+    },
+    {question: "Where is he walking into?",
+    choices: ["DisneyLand", "Sauron", "Winterfell", "Mordor"],
+    answer: "Mordor",
+    meme: "./assets/mordor.jpg"    
+    },
+    {question: "What team do I play for?",
+    choices: ["Brooklyn Nets", "Golden State Warriors", "Toronto Raptors", "Cleveland Caviliers"],
+    answer: "Brooklyn Nets",
+    meme: "./assets/mvp.jpg"    
+    },
+    {question: "I am a:",
+    choices: ["Diknowitall", "Jurassiphiser", "Sageasauras", "Philosoraptor"],
+    answer: "Philosoraptor",
+    meme: "./assets/raptor.jpg"    
+    },
+    {question: "Come with me, and you see, a world of...",
+    choices: ["Much Procastination", "Pure Imagination", "Sad Lamentation", "Infinite Experimentation"],
+    answer: "Pure Imagination",
+    meme: "./assets/willy.jpg"    
+    },
 ];
 
 
 // VARIABLES FOR DOM
-var quizReturnEl = document.querySelector("#startLink");
+var highScoresEl = document.querySelector("#highScores");
+var quizReturnEl = document.querySelector("#quizLink");
 var cardEl = document.querySelector(".card-body");
 var imageEl = document.querySelector(".card-img-top")
 var questionEl = document.querySelector(".card-title");
@@ -68,40 +69,104 @@ var finalScore;
 var highScoreStore = [];
 var quizTime = 100;
 
-// pulls high scores from local storage and inserts into to highScoreStore
-renderHighScores();
-// FUNCTION TO RENDER HIGH SCORES from local storage and push them to highScoreStore each time code is run.
+// FUNCTION TO RENDER HIGH SCORES from local storage and push them to highScoreStore
 function renderHighScores () {
+    //checks local storage for localScores item
     if(JSON.parse(localStorage.getItem("localScores")) === null) {
         return;
     }
+    // if localScores is in local storage, parses string to highScoreStore array.
     highScoreStore = JSON.parse(localStorage.getItem("localScores"));
 }
+//FUNCTION TO SORT HIGH SCORES
+function scoreSort() {
+    highScoreStore.sort(function(a, b) {
+        return b.score -a.score;
+    });
+}
+
 
 //FUNCTION TO VIEW HIGH SCORES
     function viewHighScores() { 
-    renderHighScores();    
-    // var highScoreList = localStorage.getItem("localScores");
-    // console.log("high score list " + highScoreList);
-    // var scoreObject = JSON.parse(highScoreList);
-    // console.log("score object" + scoreObject);
+    renderHighScores();
+    scoreSort();    
     cardEl.innerHTML = " ";
-    imageEl.innerHTML = " ";
-    var scoreList = document.createElement("ul");
-    scoreList.setAttribute("id","scoreList");
-    scoreList.setAttribute("class","list-group");
-    scoreList.setAttribute("style","width: 50%; margin: auto;");
-    scoreEl.appendChild(scoreList);
-    var scoreListEl = document.querySelector("#scoreList"); 
+    imageEl.setAttribute("src","./assets/titleCard.png");
+    quizReturnEl.style.display = "inline";
+    highScoresEl.style.display = "none";
+
+    // create table for high scores list
+    var scoreTable = document.createElement("table");
+    //set attributes for new element
+    scoreTable.setAttribute("id","scoreTable");
+    scoreTable.setAttribute("class","table");
+    scoreTable.setAttribute("style","width: 50%; margin: auto;");
+    // append element
+    scoreEl.appendChild(scoreTable);
+    // create variable for element to append to
+    var scoreTableEl = document.querySelector("#scoreTable");
+    
+        // create high scores header
+        var scoreHeader = document.createElement("thead");
+        //set attributes for new element
+        scoreHeader.setAttribute("id","scoreHead");
+        // append element
+        scoreTableEl.appendChild(scoreHeader);
+        // create variable for element to append to
+        var scoreHeaderEl = document.querySelector("#scoreHead");
+
+            // create table row high scores header
+            var scoreHeadRow = document.createElement("tr");
+            //set attributes for new element
+            scoreHeadRow.setAttribute("id","scoreHeadRow");
+            // scoreTable.setAttribute("class","table");
+            // scoreTable.setAttribute("style","width: 50%; margin: auto;");
+            // append element
+            scoreHeaderEl.appendChild(scoreHeadRow);
+            // create variable for element to append to
+            var scoreHeadRowEl = document.querySelector("#scoreHeadRow");
+
+                // create table headers
+                var tableHeader1 = document.createElement("th");
+                //set attributes for new element
+                tableHeader1.setAttribute("scope","col");
+                tableHeader1.textContent = "Initals";
+                // append element
+                scoreHeadRowEl.appendChild(tableHeader1);
+
+                var tableHeader2 = document.createElement("th");
+                //set attributes for new element
+                tableHeader2.setAttribute("scope","col");
+                tableHeader2.textContent = "Score";
+                // append element
+                scoreHeadRowEl.appendChild(tableHeader2);
+
+        //create table body
+        var tableBody = document.createElement("tbody");
+        tableBody.setAttribute("id", "tableBod");
+        scoreTableEl.appendChild(tableBody);
+        var tableBodyEl = document.querySelector("#tableBod");    
+
     for (var j=0; j < highScoreStore.length; j++) {
-        var scoreItem = document.createElement("li");
-        scoreItem.setAttribute("class","list-group-item list-group-item-success");
-        scoreItem.textContent = (highScoreStore.initials[j] + "      " + highScoreStore.score[j]);
-        scoreListEl.appendChild(scoreItem);
-        console.log(highScoreStore.initials[j] + "      " + highScoreStore.score[j]);
+        //create table row for each stored score
+        var tableRow = document.createElement("tr");
+        tableRow.setAttribute("id","trow"+j);
+        tableBodyEl.appendChild(tableRow);
+        var tableRowEl = document.querySelector("#trow"+j);
+        var tableDataInit = document.createElement("td");
+        tableDataInit.textContent = highScoreStore[j].initials;
+        tableRowEl.appendChild(tableDataInit);
+        var tableDataScore = document.createElement("td");
+        tableDataScore.textContent = highScoreStore[j].score;
+        tableRowEl.appendChild(tableDataScore);        
+        console.log(highScoreStore[j].initials + "      " + highScoreStore[j].score);
     }    
 }
 
+// FUNCTION TO RETURN TO QUIZ
+function reloadQuiz() {
+    location.reload();
+}    
 
 // EVENT LISTENER FOR START BUTTON
 startEl.addEventListener("click", function () {
@@ -130,7 +195,7 @@ function createChoices() {
         var choiceButton = document.createElement("button");
         choiceButton.setAttribute("type","button");
         choiceButton.setAttribute("class","btn btn-outline-secondary btn-lg btn-block");
-        choiceButton.setAttribute("style","width: 50%; margin: auto; padding: 1em");
+        choiceButton.setAttribute("style","width: 50%; margin: auto; padding: 1em; white-space: normal");
         choiceButton.setAttribute("id","choice");
         choiceButton.textContent = questions[questionNumber].choices[i];
         choicesEl.appendChild(choiceButton);
